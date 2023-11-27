d57e581c733e910695a82a1ceb78a49e6e09efa9
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2023-11-28 08:39:32 +0900

    Update ScreenShotCon.cs

--- ScreenShotCon.cs
+++ ScreenShotCon.cs
@@ -1,61 +1,26 @@
 using UnityEngine;
-using System.Collections;
-using HyperNova;
-//using UnityEngine.InputSystem;
+# if UNITY_EDITOR
+using UnityEditor;
+# endif
 using System.IO;
-using HyperNova.SaveAndLoad;
-using UnityEngine.UI;
-using System.Threading.Tasks;
-using Cysharp.Threading.Tasks;
 using System.Threading;
 using System;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
-/// スクリーンショットをキャプチャするサンプル
+/// スクリーンショットをキャプチャする
 /// </summary>
-public class ScreenShotCon : MonoBehaviour {
-    [SerializeField] Text text_captured;
-    private Color textDefaultColor;
-    private float elapsedTime = 99f;
-
+public class ScreenShotCon : MonoBehaviour
+{
     private CancellationTokenSource _cancellationTokenSource;
 
-    private void Start() {
-        //Debug.Log(this.gameObject.name, this);
-        this.textDefaultColor = this.text_captured.color;
-    }
+    private static string FolderPath => System.IO.Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal), Application.productName);
 
-    private void Update() {
-        UpdateTextUI();
-        // スペースキーが押されたら
-#if UNITY_EDITOR
-        if (ScreenShotInput()) {
-            if (!Directory.Exists(GetFilePath())) Directory.CreateDirectory(GetFilePath());
+    private static string GetFilePath()
+    {
+        var folderPath = FolderPath;
 
-            // スクリーンショットを保存
-            string filePath = System.IO.Path.Combine(GetFilePath(), GetFileName()); //GetFilePath() + GetFileName();
-            int index = 1;
-            while (true) {
-                //string _path = System.IO.Path.Combine(filePath, $"_{index}.png");
-                string _path = filePath + $"_{index}.png";
-                if (!File.Exists(_path)) {
-                    filePath = _path;
-                    break;
-                }
-            }
-            _cancellationTokenSource = new CancellationTokenSource();
-            CaptureScreenShotAsync(filePath, _cancellationTokenSource.Token).Forget();
-
-            //Task.Run(() => CaptureScreenShot(filePath));
-        }
-#endif
-    }
-
-    private string GetFilePath() {
-        return SaveDataIO._ScreenShotFolderPath;
-    }
-    private string GetFileName() {
-        string fileName = "hy_screenShot_";
+        var fileName = "screenShot_";
         fileName += System.DateTime.Now.Year.ToString() + "-";
         fileName += System.DateTime.Now.Month.ToString() + "-";
         fileName += System.DateTime.Now.Day.ToString() + "-";
@@ -63,44 +28,66 @@ public class ScreenShotCon : MonoBehaviour {
         fileName += System.DateTime.Now.Minute.ToString() + "-";
         fileName += System.DateTime.Now.Second.ToString() + "-";
         fileName += System.DateTime.Now.Millisecond.ToString() + "-";
-        return fileName;
+
+        var tempFilePath = System.IO.Path.Combine(folderPath, fileName + $"_{0}.png");
+
+        var existFileCount = Directory.GetFiles(folderPath).Length;
+        for (int i = 1; i < existFileCount; i++)
+        {
+            tempFilePath = System.IO.Path.Combine(folderPath, fileName + $"_{i}.png");
+            if (!File.Exists(System.IO.Path.Combine(folderPath, tempFilePath)))
+            {
+                break;
+            }
+        }
+        return tempFilePath;
     }
 
     // 画面全体のスクリーンショットを保存する
-    private void CaptureScreenShot(string filePath) {
-        //yield return null;
-        ScreenCapture.CaptureScreenshot(filePath);
-        Debug.Log($"ScreenShotCon/CaptureScreenShot,path:{filePath}");
-        this.elapsedTime = 0f;
+    public void StartCaptureScreenShotAsync()
+    {
+        var folderPath = FolderPath;
+        if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+        string filePath = GetFilePath();
 
+        _cancellationTokenSource = new CancellationTokenSource();
+        CaptureScreenShotAsync(filePath, _cancellationTokenSource.Token).Forget();
     }
 
-    private async UniTaskVoid CaptureScreenShotAsync(string filePath, CancellationToken cancellationToken = default(CancellationToken)) {
-        if (cancellationToken.IsCancellationRequested) {
+    private void CaptureScreenShot(string filePath)
+    {
+        ScreenCapture.CaptureScreenshot(filePath);
+        Debug.Log($"ScreenShotCon/CaptureScreenShot,path:{filePath}");
+    }
+
+    private async UniTaskVoid CaptureScreenShotAsync(string filePath, CancellationToken cancellationToken = default(CancellationToken))
+    {
+        if (cancellationToken.IsCancellationRequested)
+        {
             var ex = new OperationCanceledException(cancellationToken);
             Debug.LogError(ex);
             throw ex;
         }
-
         CaptureScreenShot(filePath);
         Debug.Log("CaptureScreenShotAsync Done!");
     }
+}
 
-    private bool ScreenShotInput() {
-        return HyperNova.InputControl.ScreenShotButton;
-    }
+# if UNITY_EDITOR
+[CustomEditor(typeof(ScreenShotCon))]
+public class ScreenShotConEditor : Editor
+{
+    private ScreenShotCon _target = null;
 
-    private void UpdateTextUI() {
-        float t0 = 0.4f;
-        int n = 3;
-        float t1 = 2f;
-        this.elapsedTime += (1f / t0) * Time.unscaledDeltaTime;
-        Color color = this.textDefaultColor;
-        if (this.elapsedTime < t0 * n) {
-            color.a = Mathf.Lerp(0, 1, (this.elapsedTime % t0) / t0);
-            color.a = Mathf.Round(color.a);
-        } else if (this.elapsedTime < t0 * n * t1) color.a = 1f;
-        else color.a = 0f;
-        this.text_captured.color = color;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        _target = target as ScreenShotCon;
+
+        if (GUILayout.Button("Capture Screen Shot!"))
+        {
+            _target.StartCaptureScreenShotAsync();
+        }
     }
 }
+# endif
